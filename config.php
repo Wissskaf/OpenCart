f987fd93f54cd0094e11d51cc4f1b008847ba901
Wissam Skaf <68133395+Wissskaf@users.noreply.github.com>
2023-03-29 13:19:16 +0300

    First Commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/ishtari/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/ishtari/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/ishtari/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/ishtari/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/ishtari/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/ishtari/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/ishtari/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/ishtari/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/ishtari/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/ishtari/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/ishtari/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/ishtari/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/ishtari/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ocdb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
